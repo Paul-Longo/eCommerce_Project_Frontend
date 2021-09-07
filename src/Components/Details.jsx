b2ab3ab108fc5207f1cef844a423b23f424a5064
spencerjamesdev <spencer.franke@gmail.com>
2021-09-07 17:52:15 -0500

    Added outline for details page

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import Product from './Product';
+
 
 export default class Details extends Component {
     render() {
+        
         return (
             <div>
-                <h3>Details this will utilize information about the products</h3>
+                <h3>Name: {Product.ProductName}</h3>
+                <h2>Price: ${Product.ProductPrice}</h2>
+                <h4>Description: {Product.ProductDescription}</h4>
+
             </div>
         )
     }

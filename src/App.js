835e29b4f2b81e61a178b28c414920c5f88de4a8
spencerjamesdev <spencer.franke@gmail.com>
2021-09-07 11:38:36 -0500

    branch Init

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Details from './Components/Details';
 import Cart from './Components/Cart';
 import Default from './Components/Default';
 import Category from './Components/Category';
+import Login from './Components/Login';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <NavBar />
+        <Login />
         <Switch>
           <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />

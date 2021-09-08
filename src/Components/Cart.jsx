7f652a7de241c0bd84b3b38e87c6148be2a3a650
Kacie <kaciehawkins1@gmail.com>
2021-09-07 22:41:19 -0600

    added handleChange and handleSubmit functions to Cart.jsx file

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
 
-export default class Cart extends Component {
+
+class Cart extends Component {
+    state = {
+        chosenProducts: [],
+    
+    }
+
+    componentDidMount(){
+        this.addItemsToCart();
+      
+    }
+
+    async addItemsToCart(){
+       let response = await axios.get('https://localhost:44394/api/cart')
+        .then(response => this.setState({
+          product: response.data
+         
+          
+          
+        }));
+        console.log('you are adding plants to cart')
+
+        return response
+        
+        }
+
+        handleChange =(event) => {
+            console.log (event.target.value)
+            this.setState({
+                
+                chosenProducts: event.target.value
+            });
+           
+        }
+        
+        handleSubmit = (event) => {
+            event.preventDefault();
+            this.props.addItemsToCart(this.state.chosenProducts);
+           
+        };
+
     render() {
         return (
             <div>
-                <h3>Cart McCart</h3>
+
+                   <button onClick={this.addItemsToCart}></button>
             </div>
-        )
+         );
+            
+        
     }
 }
+
+export default Cart;

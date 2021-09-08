553ef1d170a1c3e2af99b69546ac4909bc8d35fb
Paul-Longo <imanazarite88@gmail.com>
2021-09-08 01:32:18 -0400

    updated login.jsx. login functionality coming soon

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,26 +1,90 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Container,
+  Button,
+  Row,
+  Col,
+  Form,
+  FormControl
+} from "react-bootstrap";
+import axios from 'axios';
 
-export default class Login extends Component {
-    render() {
-        return (
-            <center>
-            <div className="login-wrapper">
-                <h1>Please Log In</h1>
-                <form>
-                    <label>
-                        <p>Username</p>
-                        <input type="text" />
-                    </label>
-                    <label>
-                        <p>Password</p>
-                        <input type="password" />
-                    </label>
-                    <div>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-            </center>
-        )
-    }
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async userLogin(props){
+      const response = await axios.post('https://localhost:44394/api/authentication/login');
+      this.setState({
+          token: response.data
+      });
+  }
+
+  handleChange(event){
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit(event){
+    event.preventDefault();
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    this.userLogin(user);
+    this.setState({
+        token: user
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col md="4">
+            <h1>Login</h1>
+            <Form>
+              <Form.Group controlId="usernameId">
+                <Form.Label>User name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="username"
+                  placeholder="Enter user name"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+                <FormControl.Feedback type="invalid"></FormControl.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="passwordId">
+                <Form.Label>Your password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+              </Form.Group>
+            </Form>
+            <Button color="primary" onClick={this.handleSubmit}>Login</Button>
+            <p className="mt-2">
+              Don't have account? <Link to="/signup">Signup</Link>
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
+
+export default Login;

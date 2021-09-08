6382e6480222160f76974a36797dce995e5ac8d0
Paul-Longo <imanazarite88@gmail.com>
2021-09-08 01:57:58 -0400

    updated default h3 tag

--- src/Components/Default.jsx
+++ src/Components/Default.jsx
@@ -4,7 +4,7 @@ export default class Default extends Component {
     render() {
         return (
             <div>
-                <h3>Page not found. it's boring right now, but the page does not exist.</h3>
+                <h3>Page not found.</h3>
             </div>
         )
     }
